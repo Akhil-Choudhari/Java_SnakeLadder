397ba90da529790d64eeaa3a2883dbdf9f4bfd71
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-27 22:52:35 +0530

    Snake and Ladder game

---
+++ UC1SnakeNLadder.java
@@ -0,0 +1,14 @@
+package SnakeLadderProgram;
+
+import java.util.Scanner;
+
+public class UC1SnakeNLadder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Snake and Ladder game");
+		System.out.println("A single player starts from the position 0(zero)");
+
+	}
+
+}
